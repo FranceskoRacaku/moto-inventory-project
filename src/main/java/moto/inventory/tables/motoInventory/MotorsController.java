f6fe77c0e6c624e8da7c8ec3fe1ea8003ed863dc
Francesko Racaku <franceskoracaku@hotmail.com>
2022-03-28 13:19:24 -0400

    tesT

--- src/main/java/moto/inventory/tables/motoInventory/MotorsController.java
+++ src/main/java/moto/inventory/tables/motoInventory/MotorsController.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping(path = "motoInventory")
+@RequestMapping("/motors")
 
 public class MotorsController {
 
