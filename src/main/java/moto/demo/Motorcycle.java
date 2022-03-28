058e132e613b0f57cd0b7af14804c96e503147c4
Francesko Racaku <franceskoracaku@hotmail.com>
2022-03-28 12:04:46 -0400

    checking

--- src/main/java/moto/demo/Motorcycle.java
+++ src/main/java/moto/demo/Motorcycle.java
@@ -2,12 +2,16 @@ package moto.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"moto.demo"})
 
 public class Motorcycle {
 
